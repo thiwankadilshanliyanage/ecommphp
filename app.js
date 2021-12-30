6abb0f57593afed6dbfacb9f3d571d03cdae5664
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-12-30 21:52:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let x = document.getElementById('login');
+let y = document.getElementById('register');
+let z = document.getElementById('btn');
+
+function register() {
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+}
+
+function login(){
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+}
