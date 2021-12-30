591c43683e264e9602bcfde2a1298c0887374df2
thiwankadilshanliyanage <79803162+thiwankadilshanliyanage@users.noreply.github.com>
2021-12-30 23:55:16 +0530

    first commit

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ function login(){
     y.style.left = '450px';
     z.style.left = '0px';
 }
+
+var state = false;
+function toggle(){
+    if(state){
+        document.getElementById("password").setAttribute("type","password");
+        state=false;
+    }else{
+        document.getElementById("password").setAttribute("type","text");
+        state=true;
+    }
+}
+
+var state2 = false;
+function toggler(){
+    if(state2){
+        document.getElementById("passwordr").setAttribute("type","password");
+        state2=false;
+    }else{
+        document.getElementById("passwordr").setAttribute("type","text");
+        state2=true;
+    }
+}
